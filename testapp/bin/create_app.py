4e1fbebc3551737174016f4fd131d72c3270a90e
saranya2612 <saranya261290@gmail.com>
2022-08-05 14:13:42 +0530

    push changes

--- testapp/bin/create_app.py
+++ testapp/bin/create_app.py
@@ -1,15 +1,20 @@
+#!/usr/bin/env python3
+
 import logging
-from flask_smorest import Api
 from flask import Flask, Blueprint
+from flask_smorest import Api
 
 import sys
 from pathlib import Path
 sys.path.append(Path(__file__).resolve().parents[1].as_posix())
-from sample_app.routes.app_config_object import blp
+from sample_app.views.app_config_object import blp
 
 app = Flask(__name__)
-app.register(blp)
+app.register_blueprint(blp)
+app.config['API_TITLE'] = 'My API'
+app.config['API_VERSION'] = 'v1'
+app.config['OPENAPI_VERSION'] = '3.0.2'
 api = Api(app)
 
 if '__name__' == '__main__':
-    app.run(host='localhost', port=8082, debug=True)
+    app.run(host='127.0.0.1', port=8082, debug=True)
