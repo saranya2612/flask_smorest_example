81d6ccc48c80a5650654313ca2437a32035bdf49
saranya2612 <saranya261290@gmail.com>
2022-08-05 16:57:12 +0530

    push changes

--- testapp/bin/create_app.py
+++ testapp/bin/create_app.py
@@ -10,11 +10,11 @@ sys.path.append(Path(__file__).resolve().parents[1].as_posix())
 from sample_app.views.app_config_object import blp
 
 app = Flask(__name__)
-app.register_blueprint(blp)
 app.config['API_TITLE'] = 'My API'
 app.config['API_VERSION'] = 'v1'
 app.config['OPENAPI_VERSION'] = '3.0.2'
 api = Api(app)
+api.register_blueprint(blp)
 
 if '__name__' == '__main__':
     app.run(host='127.0.0.1', port=8082, debug=True)
