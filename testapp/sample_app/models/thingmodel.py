ad2714d7fba12189feb528aaba415ea0ea317877
saranya2612 <saranya261290@gmail.com>
2022-08-05 16:17:51 +0530

    push changes

--- testapp/sample_app/models/thingmodel.py
+++ testapp/sample_app/models/thingmodel.py
@@ -1,11 +1,48 @@
-from pathlib import Path
-
-def jsonifyfiledata():
-    pass
 
 class Things():
     def __init__(self):
-        pass
+        self.counter = 0
+        self.things = []
 
-    def addthing(self):
-        pass
+    def getthing(self, id):
+        for thing in self.things:
+            if thing['id'] == id:
+                return thing
+
+    def creatething(self, data):
+        thing = data
+        thing['id'] = self.counter = self.counter + 1
+        self.things.append(thing)
+        return thing
+
+    def updatething(self, id, data):
+        thing = self.getthing(id)
+        thing.update(data)
+        return thing
+
+    def deletething(self, id):
+        thing = self.getthing(id)
+        self.things.remove(thing)
+
+
+DAO = Things()
+DAO.creatething({'task': 'Build an API'})
+DAO.creatething({'task': 'test api'})
+DAO.creatething({'task': 'check'})
+
+"""
+[
+  {
+    "id": 1,
+    "task": "Build an API"
+  },
+  {
+    "id": 2,
+    "task": "?????"
+  },
+  {
+    "id": 3,
+    "task": "profit!"
+  }
+]
+"""
